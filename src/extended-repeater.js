1c823ce61c3250b4626396d7861d24f6c9c61815
violettab <violettabatsura@gmail.com>
2024-12-30 21:41:35 +0300

    feat: implement Extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,50 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function repeater(str, options) {
+  let arr = [];
+  let add = "";
+  let repeat = options.repeatTimes !== undefined ? options.repeatTimes : 1;
+  let mainSeparator = options.separator !== undefined ? options.separator : "+";
+  let addSeparator =
+    options.additionSeparator !== undefined ? options.additionSeparator : "|";
+  let additionString =
+    options.addition !== undefined ? String(options.addition) : "";
+  if (additionString) {
+    add = repeater(additionString, {
+      repeatTimes: options.additionRepeatTimes,
+      separator: addSeparator,
+    });
+  }
 
+  for (let i = 0; i < repeat; i += 1) {
+    arr[i] = `${str}${add}`;
+  }
+  return arr.join(`${mainSeparator}`);
+}
+console.log(
+  repeater(true, {
+    repeatTimes: 3,
+    separator: "??? ",
+    addition: false,
+    additionRepeatTimes: 2,
+    additionSeparator: "!!!",
+  })
+);
 module.exports = {
-  repeater
+  repeater,
 };
